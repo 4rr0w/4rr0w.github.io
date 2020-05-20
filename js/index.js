304487eaa80697d222c5f6c19fbefae61d786827
Himanshu Anuragi <himanshu122122@gmail.com>
2020-05-20 15:48:34 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ var Typer={
 			var rtn= new RegExp("\n", "g"); 
 	
 			$("#console").html(text.replace(rtn,"<br/>"));
-			window.scrollBy(0,0); 
+			window.scrollBy(0,15); 
 		}
 		
 		if (key.preventDefault && key.keyCode != 122) { 
